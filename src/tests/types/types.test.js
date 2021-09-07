ba17ec4233b6604f71952f7acdaf541526ffc341
Alexson Coelho <coelho@Alexsons-MacBook-Pro.local>
2021-09-06 20:30:30 -0400

    set up test

---
+++ src/tests/types/types.test.js
@@ -0,0 +1,24 @@
+const { types } = require("../../types/types");
+
+describe("Pruebas en el types object", () => {
+  test("debe de hacer match el objeto", () => {
+    expect(types).toEqual({
+      login: "[Auth] Login",
+      logout: "[Auth] Logout",
+
+      uiSetError: "[UI] Set Error",
+      uiRemoveError: "[UI] Remove Error",
+
+      uiStartLoading: "[UI] Start loading",
+      uiFinishLoading: "[UI] Finish loading",
+
+      notesAddNew: "[Notes] New note",
+      notesActive: "[Notes] Set active note",
+      notesLoad: "[Notes] Load notes",
+      notesUpdated: "[Notes] Updated note",
+      notesFileUrl: "[Notes] Updated image url",
+      notesDelete: "[Notes] Delete note",
+      notesLogoutCleaning: "[Notes] Logout cleaning",
+    });
+  });
+});
